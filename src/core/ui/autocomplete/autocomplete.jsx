3574a09b9cb727abe8b95cf5fdad71d6b9758f06
tony.pizzicato <tony.grebnev@gmail.com>
2016-12-08 13:23:47 +0300

    lint fixes

--- src/core/ui/autocomplete/autocomplete.jsx
+++ src/core/ui/autocomplete/autocomplete.jsx
@@ -11,9 +11,9 @@ import { TextInputWithLabel } from 'ui/inputs';
 /* eslint react/sort-comp: 0 */
 
 function matchStateToTerm(item, value) {
-  const v = this.getItemValue(item) + '';
+  const v = `${this.getItemValue(item)}`.toLowerCase();
 
-  return v.toLowerCase().indexOf(value.toLowerCase()) !== -1;
+  return v.indexOf(value.toLowerCase()) !== -1;
 }
 
 type Props = {
