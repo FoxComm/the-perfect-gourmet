ba202668c49099fd07ec60cb1b475229e051a812
mbektimirov <mbektimirov@gmail.com>
2017-01-24 17:45:38 +0500

    'now' logs and env vars

--- Makefile
+++ Makefile
@@ -3,6 +3,7 @@ DOCKER_IMAGE ?= tpg-storefront
 DOCKER_TAG ?= master
 BRANCH_NAME_ESCAPED = $(shell git name-rev --name-only HEAD | sed 's/[^a-zA-Z0-9]/-/g')
 PROJECT_PREFIX = tpg
+NOW_ALIAS = $(PROJECT_PREFIX)-$(BRANCH_NAME_ESCAPED)
 
 dev d:
 	source .env && yarn dev
@@ -26,9 +27,11 @@ clean:
 test: setup
 	yarn test
 
-deploy-now:
-	now ./hello-now
-	cd hello-now && ../node_modules/.bin/babel-node now-realias.js $(PROJECT_PREFIX)-$(BRANCH_NAME_ESCAPED).now.sh
+now-deploy:
+	now ./hello-now -e API_URL=https://httpbin.org/ -e LOG_STREAM=$(NOW_ALIAS)
+	cd hello-now && ../node_modules/.bin/babel-node now-realias.js $(NOW_ALIAS).now.sh
 
+now-logs:
+	now-logs $(NOW_ALIAS)
 
 .PHONY: dev d setup build docker docker-push clean test deploy-now
--- hello-now/server.js
+++ hello-now/server.js
@@ -1,8 +1,32 @@
 const express = require('express');
+const superagent = require('superagent');
 const app = express();
 
+const apiUrl = process.env.API_URL;
+const logStream = process.env.LOG_STREAM;
+
+require('now-logs')(logStream);
+
 app.get('/', function (req, res) {
-  res.send('<h1>Hello World!</h1>');
+  console.log('Request to /', logStream, apiUrl);
+
+  superagent
+    .post(`${apiUrl}/post`)
+    .send({ apiUrl, logStream })
+    .set('Accept', 'application/json')
+    .end((err, response) => {
+      if (err || !response.ok) {
+        console.log(`Error occured while making a call to ${apiUrl}/post`);
+      } else {
+        console.log(`Post result to ${apiUrl}/post:`, JSON.stringify(response.body));
+      }
+    });
+
+  res.send(`
+    <h1>Hello Wrld!</h1>
+    <h2>API_URL: ${apiUrl}</h2>
+    <h2>LOG_STREAM: ${logStream}</h2>
+  `);
 });
 
 app.listen(3000, function () {
