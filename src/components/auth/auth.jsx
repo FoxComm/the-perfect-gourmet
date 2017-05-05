f7dbb1b35497cb737c3fa649177798ac95beef06
Artem <artem@foxcommerce.com>
2017-05-05 15:26:01 +0300

    Add footer

--- src/components/auth/auth.jsx
+++ src/components/auth/auth.jsx
@@ -3,25 +3,29 @@ import React, { Component } from 'react';
 // components
 import Login from './login';
 import Signup from './signup';
+import Footer from '../footer/footer';
 
 import styles from './auth-page.css';
 
 class Auth extends Component {
   render() {
     return (
-      <div styleName="forms">
-        <div styleName="auth-block">
-          <Login />
+      <div styleName="container">
+        <div styleName="forms">
+          <div styleName="auth-block">
+            <Login />
+          </div>
+          <div styleName="divider" />
+          <div styleName="mobile-divider-block">
+            <div styleName="mobile-divider" />
+            <p>or</p>
+            <div styleName="mobile-divider" />
+          </div>
+          <div styleName="auth-block">
+            <Signup />
+          </div>
         </div>
-        <div styleName="divider" />
-        <div styleName="mobile-divider-block">
-          <div styleName="mobile-divider" />
-          <p>or</p>
-          <div styleName="mobile-divider" />
-        </div>
-        <div styleName="auth-block">
-          <Signup />
-        </div>
+        <Footer />
       </div>
     );
   }
