aa57444304cd0f1ee877f92d8e6af26aaed8a9b1
Artem <artem@foxcommerce.com>
2017-05-05 15:56:15 +0300

    Add header to the page

--- src/components/auth/auth.jsx
+++ src/components/auth/auth.jsx
@@ -1,16 +1,40 @@
+/* @flow */
 import React, { Component } from 'react';
+
+// libs
+import _ from 'lodash';
+import { connect } from 'react-redux';
 
 // components
 import Login from './login';
 import Signup from './signup';
 import Footer from '../footer/footer';
+import Header from '../header/header';
+
+// actions
+import { closeBanner } from 'modules/banner';
 
 import styles from './auth-page.css';
 
+type Props = {
+  location: Object,
+  isBannerVisible: boolean,
+  closeBanner: Function,
+};
+
 class Auth extends Component {
+  props: Props;
+
   render() {
+    const { location, isBannerVisible } = this.props;
     return (
       <div styleName="container">
+        <Header
+          path={location.pathname}
+          query={location.query}
+          isBannerVisible={isBannerVisible}
+          closeBanner={this.props.closeBanner}
+        />
         <div styleName="forms">
           <div styleName="auth-block">
             <Login />
@@ -31,4 +55,12 @@ class Auth extends Component {
   }
 }
 
-export default Auth;
+const mapStateToProps = (state) => {
+  return {
+    isBannerVisible: _.get(state.banner, 'isVisible', false),
+  };
+};
+
+export default connect(mapStateToProps, {
+  closeBanner,
+})(Auth);
